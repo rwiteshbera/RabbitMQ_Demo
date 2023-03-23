5d3246095d065c8d9c851eb8ad2d8dfaf5a07019
rwiteshbera <rwiteshbera@gmail.com>
2023-03-23 23:38:16 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rwiteshbera/microservices_demo/rabbitMQ
+
+go 1.20
+
+require github.com/streadway/amqp v1.0.0 // indirect
---
+++ producer.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s: %s", message, err.Error())
+		return
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:15672")
+	failOnError(err, "Failed to connect with RabbitMQ.")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel.")
+	defer ch.Close()
+
+	// Create a queue to send the message
+	queue, err := ch.QueueDeclare(
+		"go-queue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue.")
+
+	// Set the payload
+	body := "Golang is awesome"
+	err = ch.Publish(
+		"",
+		queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	failOnError(err, "Failed to publish a message")
+	log.Println("Sending message: ", body)
+}
